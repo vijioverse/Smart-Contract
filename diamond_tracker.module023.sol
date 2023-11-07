788c8764aadfa52a648b457a92cb75d630543f57
vijioverse <133581739+vijioverse@users.noreply.github.com>
2023-11-08 00:35:58 +0530

    Update diamond_tracker.module023.sol

--- diamond_tracker.module023.sol
+++ diamond_tracker.module023.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
 
+contract DiamondLedger {
+    mapping (uint256 => uint256) public diamonds;
+    
+    //this function imports the diamonds (range: 0 <= weight <=1000)
+    function importDiamonds(uint[] calldata weights) public {
+        for (uint256 i; i < weights.length; i++) {
+            diamonds[weights[i]]++;
+        }
+    }
+
+    //this function returns the total number of available diamonds as per the weight
+    function availableDiamonds(uint weight) public view returns(uint) {
+        return diamonds[weight];
+    }
+}
