f6c08e1576e559945a02fa604235c72e33df798f
vijioverse <133581739+vijioverse@users.noreply.github.com>
2023-11-08 00:32:59 +0530

    Update lucky_draw_module016.sol

--- Lucky_draw_module016.sol
+++ Lucky_draw_module016.sol
@@ -1,1 +1,159 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AutomationCompatibleInterface.sol";
+
+error Jackpot__UpkeepNotNeeded(uint256 currentBalance, uint256 numPlayers, uint256 raffleState);
+error Jackpot__TransferFailed();
+error Jackpot__SendMoreToEnterRaffle();
+error Jackpot__RaffleNotOpen();
+
+contract Jackpot is VRFConsumerBaseV2, AutomationCompatibleInterface {
+    enum JackpotState {
+        OPEN,
+        CALCULATING
+    }
+
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    uint64 private immutable i_subscriptionId;
+    bytes32 private immutable i_gasLane;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+    uint256 private immutable i_interval;
+    uint256 private immutable i_entranceFee;
+    uint256 private s_lastTimeStamp;
+    address private s_recentWinner;
+    address payable[] private s_players;
+    JackpotState private s_jackpotstate;
+
+    event RequestedJackpotWinner(uint256 indexed requestId);
+    event JackpotEnter(address indexed player);
+    event WinnerPicked(address indexed player);
+
+    constructor(
+        address vrfCoordinatorV2,
+        uint64 subscriptionId,
+        bytes32 gasLane,
+        uint256 interval,
+        uint256 entranceFee,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_interval = interval;
+        i_subscriptionId = subscriptionId;
+        i_entranceFee = entranceFee;
+        s_raffleState = JackpotState.OPEN;
+        s_lastTimeStamp = block.timestamp;
+        i_callbackGasLimit = callbackGasLimit;
+    }
+
+    function enterJackpot() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Jackpot__SendMoreToEnterJackpot);
+        }
+        if (s_jackpotState != JackpotState.OPEN) {
+            revert Jackpot__JackpotNotOpen();
+        }
+        s_players.push(payable(msg.sender));
+        emit JackpotEnter(msg.sender);
+    }
+    function checkUpkeep(
+        bytes memory
+    )
+        public
+        view
+        override
+        returns (
+            bool upkeepNeeded,
+            bytes memory
+        )
+    {
+        bool isOpen = JackpotState.OPEN == s_jackpotState;
+        bool timePassed = ((block.timestamp - s_lastTimeStamp) > i_interval);
+        bool hasPlayers = s_players.length > 0;
+        bool hasBalance = address(this).balance > 0;
+        upkeepNeeded = (timePassed && isOpen && hasBalance && hasPlayers);
+        return (upkeepNeeded, "0x0"); 
+    }
+
+    function performUpkeep(
+        bytes calldata
+    ) external override {
+        (bool upkeepNeeded, ) = checkUpkeep("");
+        if (!upkeepNeeded) {
+            revert Jackpot__UpkeepNotNeeded(
+                address(this).balance,
+                s_players.length,
+                uint256(s_jackpotState)
+            );
+        }
+        s_jackpotState = JackpotState.CALCULATING;
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+        emit RequestedJackpotWinner(requestId);
+    }
+    function fulfillRandomWords(
+        uint256,
+        uint256[] memory randomWords
+    ) internal override {
+      
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recentWinner = recentWinner;
+        s_players = new address payable[](0);
+        s_jackpotState = JackpotState.OPEN;
+        s_lastTimeStamp = block.timestamp;
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Jackpot__TransferFailed();
+        }
+        emit WinnerPicked(recentWinner);
+    }
+
+
+    function getJackpotState() public view returns (JackpotState) {
+        return s_jackpotState;
+    }
+
+    function getNumWords() public pure returns (uint256) {
+        return NUM_WORDS;
+    }
+
+    function getRequestConfirmations() public pure returns (uint256) {
+        return REQUEST_CONFIRMATIONS;
+    }
+
+    function getRecentWinner() public view returns (address) {
+        return s_recentWinner;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+
+    function getLastTimeStamp() public view returns (uint256) {
+        return s_lastTimeStamp;
+    }
+
+    function getInterval() public view returns (uint256) {
+        return i_interval;
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getNumberOfPlayers() public view returns (uint256) {
+        return s_players.length;
+    }
+}
